c1e07c771f06fe46a53ce6c26fa68e36bc18a12d
jamesobrien86 <jamesobrien.web@gmail.com>
2019-06-28 13:10:22 +0300

    impl - basic setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Redux from 'redux';
+import ReactRedux from 'react-redux';
+
+import App from './components/App';
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
